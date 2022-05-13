847500e485508dd1173d43501da2bd2549c1d870
Seyda Balci <seydaseymabalci16@gmail.com>
2022-05-14 02:15:19 +0300

    Initial Commit

---
+++ PersonalMoneyTracker/PersonalMoneyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalMoneyTrackerApp.swift
+//  PersonalMoneyTracker
+//
+//  Created by Şeyda Şeyma Balcı on 14.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonalMoneyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
